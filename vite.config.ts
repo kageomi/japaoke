d2c4944f9f0997af25807eaeddc91cca5a12f782
蔭山忠臣 <k.tadaomi@gmail.com>
2022-11-01 13:55:28 +0100

    fix invalid asset path bug regarding vite config base path

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/japaoke/'
+export default defineConfig(({ command, mode, ssrBuild }) => {
+  const config = {
+    plugins: [react()]
+  }
+  if (command === 'build') {
+    return {
+      ...config,
+      base: '/japaoke/'
+    }
+  }
+  return config
 })
