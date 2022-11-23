0392c17462e01fd80b26b2b2eceafbdab5e6340b
蔭山忠臣 <k.tadaomi@gmail.com>
2022-11-23 13:21:06 +0100

    implement footer

--- src/components/organismus/Footer.tsx
+++ src/components/organismus/Footer.tsx
@@ -1,8 +1,27 @@
 import { FC } from 'react';
-import { Box } from '@chakra-ui/react';
+import { Flex, Box, BoxProps, Text } from '@chakra-ui/react';
 
-const Footer: FC = () => {
-  return <Box>this is footer</Box>;
+const Footer: FC<BoxProps> = (props) => {
+  return (
+    <Box {...props}>
+      <footer>
+        <Flex
+          fontSize="0.7em"
+          padding="2em"
+          bgColor="#fff"
+          borderTop="1px"
+          borderColor="#fdfdfd"
+          color="gray.500"
+          justifyContent="space-around"
+          flexWrap="wrap"
+        >
+          <Text>powered by yahoo</Text>
+          <Text>powered by genius</Text>
+          <Text>©2022 kageomi</Text>
+        </Flex>
+      </footer>
+    </Box>
+  );
 };
 
 export default Footer;
