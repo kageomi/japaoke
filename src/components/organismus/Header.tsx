905f0c9d462316ea79bddff06da104cdc02e7864
蔭山忠臣 <k.tadaomi@gmail.com>
2022-11-23 13:20:50 +0100

    implement header

--- src/components/organismus/Header.tsx
+++ src/components/organismus/Header.tsx
@@ -1,8 +1,24 @@
 import { FC } from 'react';
-import { Box } from '@chakra-ui/react';
+import { Box, BoxProps } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
+import { Logo } from '../atoms/Logo';
 
-const Header: FC = () => {
-  return <Box>JAPAOKE</Box>;
+const Header: FC<BoxProps> = (props) => {
+  return (
+    <Box
+      paddingX="2em"
+      paddingY="0.2em"
+      boxShadow="sm"
+      bgColor="white"
+      {...props}
+    >
+      <header>
+        <Link to="/">
+          <Logo isSmall />
+        </Link>
+      </header>
+    </Box>
+  );
 };
 
 export default Header;
