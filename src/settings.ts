045f332459d5caaa4d26dfa0300830ac90fbe690
蔭山忠臣 <k.tadaomi@gmail.com>
2022-11-25 14:02:15 +0100

    add links into the footer

---
+++ src/settings.ts
@@ -0,0 +1,6 @@
+const GENIUS_URL = 'https://docs.genius.com/';
+const YAHOO_URL =
+  'https://developer.yahoo.co.jp/webapi/jlp/furigana/v2/furigana.html';
+const GITHUB_URL = 'https://github.com/kageomi';
+
+export { GENIUS_URL, YAHOO_URL, GITHUB_URL };
