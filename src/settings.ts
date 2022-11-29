a6dee27ac878607987ec809c84ca13d9e533a482
蔭山忠臣 <k.tadaomi@gmail.com>
2022-11-29 11:37:36 +0100

    add basename

--- src/settings.ts
+++ src/settings.ts
@@ -1,7 +1,8 @@
 const API_URL = import.meta.env.VITE_API_URL;
+const ROUTER_BASENAME = import.meta.env.VITE_ROUTER_BASENAME;
 const GENIUS_URL = 'https://docs.genius.com/';
 const YAHOO_URL =
   'https://developer.yahoo.co.jp/webapi/jlp/furigana/v2/furigana.html';
 const GITHUB_URL = 'https://github.com/kageomi';
 
-export { API_URL, GENIUS_URL, YAHOO_URL, GITHUB_URL };
+export { API_URL, GENIUS_URL, YAHOO_URL, GITHUB_URL, ROUTER_BASENAME };
